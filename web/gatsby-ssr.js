cd8a1962b436acd6b6aa51a7ff1d6ac09aa36037
Casey K <tehcnicolour@gmail.com>
2025-12-05 19:41:45 -0800

    add meta pixel

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -1,1 +1,34 @@
+import React from 'react'
+
 export { wrapPageElement, wrapRootElement } from './gatsby-browser'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="fb-pixel"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '101952413626021');
+          fbq('track', 'PageView');
+        `,
+      }}
+    />,
+    <noscript key="fb-pixel-noscript">
+      <img
+        height="1"
+        width="1"
+        style={{ display: 'none' }}
+        src="https://www.facebook.com/tr?id=101952413626021&ev=PageView&noscript=1"
+      />
+    </noscript>,
+  ])
+}
